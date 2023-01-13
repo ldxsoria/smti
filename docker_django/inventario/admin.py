5de8f32c05e0f576f16b07aaaade40cbc5343283
ldxsoria <ldxsoria@gmail.com>
2023-01-12 22:29:02 -0500

    Agregando modelos en inventario

--- docker_django/inventario/admin.py
+++ docker_django/inventario/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Proveedor, Banco, Tipo_moneda, Banco_Proveedor, Contacto_Proveedor
 
 # Register your models here.
+
+# Register your models here.
+admin.site.register(Proveedor)
+admin.site.register(Tipo_moneda)
+admin.site.register(Banco)
+admin.site.register(Banco_Proveedor)
+admin.site.register(Contacto_Proveedor)
--- docker_django/inventario/models.py
+++ docker_django/inventario/models.py
@@ -5,37 +5,57 @@ from django.db import models #DJANGO DEFAULT
 
 #Proveedor
 class Proveedor(models.Model):
-    ruc = models.BigIntegerField(primary_key)
-    razon = models.TextField(max_length=120, blank=True)
-    correo = models.EmailField(max_length=254, **options)
-    telefono = models.BigIntegerField(max_length=9)
+    ruc = models.BigIntegerField(primary_key=True)
+    razon = models.TextField(max_length=120, blank=True, null=True)
+    apodo = models.TextField(max_length=120, blank=True, null=True)
+    correo = models.EmailField(max_length=254, blank=True, null=True)
+    telefono = models.BigIntegerField(max_length=9, blank=True, null=True)
 
-"""
+    def __str__(self):
+        return self.razon
+
+
 #tipo_moneda
 class Tipo_moneda(models.Model):
-    cod_moneda = models.TextField(max_length=5, primary_key)
-    pais = models.TextField(max_length=40)
-    simbolo_monetario = models.TextField(max_length=3)
+    cod_moneda = models.CharField(max_length=5, primary_key=True)
+    pais = models.CharField(max_length=40)
+    simbolo_monetario = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.cod_moneda
+
 
 #banco
 class Banco(models.Model):
-    nombre = models.TextField(max_length=60)
+    nombre = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nombre
 
 #Cuentas bancarias
-class Banco_Proveedor
-    ruc = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+class Banco_Proveedor(models.Model):
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
     cod_banco = models.OneToOneField(Banco, on_delete=models.CASCADE)
     moneda = models.OneToOneField(Tipo_moneda, on_delete=models.CASCADE)
-    nombre_cuenta = models.TextField(max_length=120, blank=True)
+    nombre_cuenta = models.CharField(max_length=120, blank=True)
     cuenta  = models.BigIntegerField(max_length=30)
     cci =  models.BigIntegerField(max_length=35)
 
-#Proveedor contacto
-ruc
-nombre_contacto
-apellido_contacto
-telefono
+    def __str__(self):
+        return f'{self.proveedor} - {self.cod_banco}'
 
+#Proveedor contacto
+class Contacto_Proveedor(models.Model):
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    nombre_contacto = models.CharField(max_length=120, blank=True)
+    apellido_contacto = models.CharField(max_length=120, blank=True)
+    telefono = models.BigIntegerField(max_length=35)
+
+    def __str__(self):
+        return f'{self.nombre_contacto} - {self.proveedor.apodo}'
+    
+
+"""  
 #Factura
 ruc
 num_factura
